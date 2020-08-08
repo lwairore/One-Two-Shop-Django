d39516b2d4077c06e5c18d68fb7c24d6e27b1f90
lwairore <kwairore@gmail.com>
2020-08-08 18:21:54 +0300

    Create product_detail view function to retrieve and display a single product.

--- shop/views.py
+++ shop/views.py
@@ -16,4 +16,11 @@ def product_list(request, category_slug=None):
                    'products': products})
 
 
-                    
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})                        
