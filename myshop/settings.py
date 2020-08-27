072a301fc9e55fc9b6bffa074f9ab96f04e071c7
lwairore <kwairore@gmail.com>
2020-08-27 08:32:02 +0300

    - Configure static files and media files.

--- myshop/settings.py
+++ myshop/settings.py
@@ -92,8 +92,15 @@ if DEBUG:
     }
 
     STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+
     
     MEDIA_URL = '/media/'
+
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
